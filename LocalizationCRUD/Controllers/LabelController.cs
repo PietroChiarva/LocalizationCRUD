7a4987205fcc7d6c04c6e4652c543e88fdd2f82a
Pietro Chiarva <pietro.chiarva@edu.itspiemonte.it>
2018-02-08 10:34:12 +0100

    first commit

---
+++ LocalizationCRUD/Controllers/LabelController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LocalizationCRUD.Controllers
+{
+    public class LabelController : Controller
+    {
+        // GET: Label
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+
+
+}
